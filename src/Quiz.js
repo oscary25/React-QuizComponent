6bd62d3d5bf5badb0853fdd461643214aeb0f642
Oscary Garcia <oscarygarcia@tecnologiateradata15.com>
2020-08-16 20:05:40 -0400

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,12 @@ class Quiz extends Component {
       quiz_position: 1,
     };
   }
+
+  showNextQuestion() {
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1 };
+    });
+  }
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -22,6 +28,7 @@ class Quiz extends Component {
           <QuizEnd />
         ) : (
           <QuizQuestion
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
             quiz_question={
               quizData.quiz_questions[this.state.quiz_position - 1]
             }
--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 
 class QuizQuestionButton extends Component {
+  handleClick() {
+    this.props.clickHandler(this.props.button_text);
+  }
   render() {
     return (
-      <div>
-        <li>
-          <button>{this.props.button_text}</button>
-        </li>
-      </div>
+      <li>
+        <button onClick={this.handleClick.bind(this)}>
+          {this.props.button_text}
+        </button>
+      </li>
     );
   }
 }
